999f6aa6cf189904958b290f5be06604326a5a59
joson <josonyuu@163.com>
2019-12-09 13:59:28 +0800

    update class

--- src/main/java/com/joson/TestController/Controller.java
+++
@@ -1,4 +0,0 @@
-package com.joson.TestController;
-
-public class Controller {
-}
---
+++ src/main/java/com/joson/TestController/MyController.java
@@ -0,0 +1,11 @@
+package com.joson.TestController;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void printStr(){
+        System.out.println("hello");
+    }
+}
